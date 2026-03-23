7941c126bcfae7e0692c1de6fe6ee3dddf0349f6
Roman <romacergees15838@gmail.com>
2026-03-23 15:39:48 +0300

    Add files via upload

---
+++ Step1/Program.cs
@@ -0,0 +1,78 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Step1
+{
+    internal class step1
+    {
+        string _connectionString;
+        public class UserWithOrdersDto
+        {
+            // реализация класса
+            public int userId;
+            public int userName;
+            public List<OrderDto>orders;// для примера
+        }
+        public class OrderDto
+        {
+            public int OrderId { get; set; }
+            public decimal Total { get; set; }
+        }
+        public List<UserWithOrdersDto> GetUserWithOrders()
+        {
+            var result = new List<UserWithOrdersDto>();
+            //1 using для автоматического закрытия
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                //2 защита от sql инъекций (параметризированный запрос)
+                const string usersSql = "SELECT Id, Name FROM Users";
+
+                using (var userCmd = new SqlCommand(usersSql, connection))
+                using (var usersReader = userCmd.ExecuteReader())
+                {
+                    while (usersReader.Read())
+                    {
+                        var userId = usersReader.GetInt32(usersReader.GetOrdinal("Id"));
+                        var userName = usersReader.GetString(usersReader.GetOrdinal("Name"));
+                        //Все еще N+1 проблема, но ресурсы теперь закрываются
+                        const string orderSql = "SELECT Id, Total FROM Orders WHERE UserID=@UserId";
+                        using (var ordersCmd = new SqlCommand(orderSql, connection))
+                        {
+                            ordersCmd.Parameters.AddWithValue("@userId", userId);
+                            using (var ordersReader = ordersCmd.ExecuteReader())
+                            {
+                                var orders = new List<OrderDto>();
+                                while (ordersReader.Read())
+                                {
+                                    orders.Add(new OrderDto
+                                    {
+                                        OrderId = ordersReader.GetInt32(ordersReader.GetOrdinal("Id")),
+                                        Total = ordersReader.GetDecimal(ordersReader.GetOrdinal("Total")),
+                                    });
+                                }
+                                result.Add(new UserWithOrdersDto
+                                {
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
+
+
